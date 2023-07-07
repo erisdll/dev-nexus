1deea2351999f8f96dbf0d855f9b49ee6cc4e45f
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-07-07 13:26:37 -0300

    Created Lang.js model

--- api/src/models/Lang.js
+++ api/src/models/Lang.js
@@ -1,10 +1,54 @@
 const mongoose = require('mongoose');
 
 const langSchema = new mongoose.Schema({
+  // Main Fields
   name: {
     type: String,
     required: true,
     unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: 1000,
+    trim: true,
+  },
+  
+  // Array fields
+  platforms: {
+    type: [String],
+    max: 10,
+    required: true,
+  },
+  bestFor: {
+    type: [String],
+    required: true,
+  },
+  advantages: {
+    type: [String],
+    required: true,
+  },
+  disadvantages: {
+    type: [String],
+    required: true,
+  },
+  designedBy: {
+    type: [String],
+    required: true,
+  },
+  difficulty: {
+    type: String,
+    required: true,
+  },
+
+  // Number fields
+  yearCreated: {
+    type: Number,
+    required: true,
+  },
+  popularity: {
+    type: Number,
+    default: 0,
   },
 });
 
