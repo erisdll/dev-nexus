d1eb047c6452050b1f6ed476aae977a7877f751f
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-07-09 13:54:44 -0300

    updated models

--- src/models/User.js
+++ src/models/User.js
@@ -1,59 +1,57 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-  {
-    // Main fields
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    bio: {
-      type: String,
-      maxlength: 1000,
-      trim: true,
-    },
-    imgURL: {
-      type: String,
-    },
-
-    // Selections
-    selectedLangs: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Lang',
-      },
-    ],
-    selectedTechs: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Tech',
-      },
-    ],
-    selectedSkills: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Area',
-      },
-    ],
-
-    // Control fields
-
-    deactivated: {
-      type: Boolean,
-      default: false,
-    },
+const userSchema = new mongoose.Schema({
+  // Main fields
+  username: {
+    type: String,
+    required: true,
+    unique: true,
   },
-);
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  bio: {
+    type: String,
+    maxlength: 1000,
+    trim: true,
+  },
+  imgURL: {
+    type: String,
+  },
+
+  // Selections
+  selectedLangs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Lang',
+    },
+  ],
+  selectedTechs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Tech',
+    },
+  ],
+  selectedAreas: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Area',
+    },
+  ],
+
+  // Control fields
+
+  deactivated: {
+    type: Boolean,
+    default: false,
+  },
+});
 
 const User = mongoose.model('User', userSchema);
 
