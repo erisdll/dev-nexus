9d412a8dd394b135e5ea50e4cda747036ac9736e
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-07-26 18:08:52 -0300

    updated swagger

--- tools/swagger.js
+++ tools/swagger.js
@@ -1,4 +1,27 @@
 const swaggerAutogen = require('swagger-autogen')();
 const outputFile = './swagger_output.json'
 const endpointsFiles = ['../src/app.js']
-swaggerAutogen(outputFile, endpointsFiles)
+
+const config = {
+  info: {
+    version: '1.0.0',
+    title: 'devNexus',
+    description:
+      'The devNexus API provides users around the world with a way to keep track of their favorite programming languages, areas of interest and technologies, as well as staying up to date with the communities favorite resources.',
+  },
+  host: process.env.VIRTUAL_HOST || 'localhost',
+  basePath: '/',
+  schemes: ['https', 'http'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  securityDefinitions: {
+    Bearer: {
+      type: 'apiKey',
+      in: 'header',
+      name: 'Authorization',
+      description: 'Please enter a valid token to test the requests below...',
+    },
+  },
+};
+
+swaggerAutogen(outputFile, endpointsFiles, config)
