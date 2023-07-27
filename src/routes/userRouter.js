7ab809bba2bb7569e1764f4db5e29291872e16d4
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-07-27 00:26:42 -0300

    reordered user routes

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const { authenticateUser, isAdmin } = require('../utils/authenticator');
 
+// Profile & Settings Routes 
+// These routes allow access to an authenticated user's private data.
+// They enable them to manage >only their own< profile and settings.
+router.get('/selections', authenticateUser, userController.getUserSelections);
+router.patch('/selections', authenticateUser, userController.updateUserSelections);
+router.get('/profile', authenticateUser, userController.getUserProfile);
+router.patch('/profile', authenticateUser, userController.updateUserProfile);
+router.patch('/deactivate', authenticateUser, userController.deactivateAcc);
+
 // Public Routes
 // These routes are accessible to any user.
 router.get('/all', userController.getAllUsers);
@@ -13,14 +22,5 @@ router.get('/:username', userController.getUser);
 router.post('/add', authenticateUser, isAdmin, userController.createUser);
 router.patch('/:username', authenticateUser, isAdmin, userController.updateUser);
 router.delete('/:username', authenticateUser, isAdmin, userController.deleteUser);
-
-// Profile & Settings Routes 
-// These routes allow access to an authenticated user's private data.
-// They enable them to manage >only their own< profile and settings.
-router.get('/selections', authenticateUser, userController.getUserSelections);
-router.patch('/selections', authenticateUser, userController.updateUserSelections);
-router.get('/profile', authenticateUser, userController.getUserProfile);
-router.patch('/profile', authenticateUser, userController.updateUserProfile);
-router.patch('/deactivate', authenticateUser, userController.deactivateAcc);
 
 module.exports = router;
